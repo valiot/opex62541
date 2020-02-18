99918a094f4114b6dae57796d54ec736495d28d5
alde103 <aldebaran_103@hotmail.com>
2020-02-18 16:24:51 -0600

    Deps added

--- mix.exs
+++ mix.exs
@@ -7,6 +7,7 @@ defmodule Opex62541.MixProject do
       version: "0.1.0",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
+      cmake_lists: "src/CMakeLists.txt",
       deps: deps()
     ]
   end
@@ -21,6 +22,7 @@ defmodule Opex62541.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:elixir_cmake, github: "valiot/elixir-cmake", branch: "dev"},
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
